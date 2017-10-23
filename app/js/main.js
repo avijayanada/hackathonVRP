193c60ed0576eba036312b281f24fc2b6754b726
avijayanada <v-avijayanada@expedia.com>
2017-10-23 21:16:42 +0530

    Basic completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-(function(){
-    'use strict';
-    console.log("Test");
-})();
+AFRAME.registerComponent('main', {
+    schema: {},
+    init: function () {
+        console.log('init');
+    },
+    update: function () {
+        console.log('update init');
+    },
+    remove: function () {
+        console.log('remove');
+    },
+    pause: function () {
+        console.log('pause');
+    },
+    play: function () {
+        console.log('play');
+    }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('copy-html-files', function () {
 gulp.task('connect', function () {
   connect.server({
     root: 'app/',
-    port: 8888
+    port: 7777
   });
 });
 gulp.task('connectDist', function () {
